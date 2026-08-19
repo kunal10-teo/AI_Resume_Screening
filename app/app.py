37a6e0d41d85c591a82b3ec1066d22c45b3e1de4
Kunal Teotia <teotiakunal497@gmail.com>
2026-08-19 12:26:56 +0530

    Fix project imports

--- app/app.py
+++ app/app.py
@@ -24,13 +24,15 @@ sys.path.append(
 # ============================================================
 # PROJECT IMPORTS
 # ============================================================
+
 from semantic_matcher import semantic_match
 
 from pdf_report import create_report
 
-from database import save_candidate, get_candidates
-
-
+from database import (
+    save_candidate,
+    get_candidates
+)
 
 from ranking import (
     calculate_final_ranking_score,
